bf03045b21ddf3209c43d449a12010bf73a5ffc2
김성제 <rlatjdwp0211@naver.com>
2021-12-29 15:59:52 +0900

    User 모델 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -15,8 +15,17 @@ class UserStorage {
             }
             return newUsers;
         }, {});
-        
         return newUsers;
+    }
+    static getUserInfo(id) {
+        const users = this.#users;
+        const idx = users.id.indexOf(id);
+        const usersKeys = Object.keys(users);
+        const userInfo = usersKeys.reduce((newUser, info) => {
+            newUser[info] = users[info][idx];
+            return newUser;
+        }, {});
+        return userInfo;
     }
 }
 
