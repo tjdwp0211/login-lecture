296e3a49c614b142063b5b42a5fb25b774e1c7a1
김성제 <rlatjdwp0211@naver.com>
2021-12-29 11:41:08 +0900

    백엔드에서 로그인 기능 구현

--- app/app.js
+++ app/app.js
@@ -2,9 +2,8 @@
 
 // 모듈
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
-
-const PORT = 3000;
 
 // 라우팅
 const home = require("./src/routes/home");
@@ -15,6 +14,9 @@ app.set("views", "./src/views");
 app.set("view engine", "ejs");
 // 앱 세팅
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+// 20line = URL을 통해 전달되는 데이터에 한글, 공백 등과 같은 문자가 포함될 경우 제대로 인식되지 않는 문제 해결
 app.use(express.static(`${__dirname}/src/public`));
 app.use("/", home) // use -> 미드 웨어를 등록해주는 메서드.
 
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,16 +1,43 @@
 "user strict";
 
-const home = (req, res) => {
-    res.render("home/index");
+const users = {
+    id: ["kimsungje", "응애", "hihihi"],
+    password: ["1234", "1234", "123456"],
 };
 
-const login = (req, res) => {
-    res.render("home/login");
-}
+const output = {
+    home: (req, res) => {
+        res.render("home/index");
+    },    
+    login: (req, res) => {
+        res.render("home/login");
+    },
+};
+
+const process = {
+    login: (req, res) => {
+        const id = req.body.id;
+        const password = req.body.password;
+
+        if (users.id.includes(id)) {
+            const idx = users.id.indexOf(id);
+            if (users.password[idx] === password) {
+                return res.json({
+                    success: true,
+                });
+            }
+        }
+
+        return res.json({
+            success: false,
+            msg: "로그인에 실패하셨습니다."
+        })
+    },
+};
 
 module.exports = {
-    home,
-    login,
+    output,
+    process,
 }
 
 
