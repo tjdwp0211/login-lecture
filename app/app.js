a27b86244bd13e6a85ee9b3751a5a8e005a97392
김성제 <rlatjdwp0211@naver.com>
2021-12-29 10:40:55 +0900

    프런트 단 javasctipt 파일 만들기

--- app/app.js
+++ app/app.js
@@ -5,14 +5,17 @@ const express = require("express");
 const app = express();
 
 const PORT = 3000;
-// 라우딩
+
+// 라우팅
 const home = require("./src/routes/home");
+// 라우팅
 
 // 앱 세팅
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 // 앱 세팅
 
+app.use(express.static(`${__dirname}/src/public`));
 app.use("/", home) // use -> 미드 웨어를 등록해주는 메서드.
 
 module.exports = app;
