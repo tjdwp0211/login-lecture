dae265cd0427e884d75937fb41d163495e19e822
김성제 <rlatjdwp0211@naver.com>
2021-12-30 10:58:56 +0900

    회원가입 라우팅 구현

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -9,6 +9,9 @@ const registerBtn = document.querySelector("#button");
 registerBtn.addEventListener("click", register);
 
 function register() {
+    if (!id.value) return alert("아이디를 입력해주세요.");
+    if (password !== confirmPassword) return alert("비밀번호가 일치하지 않습니다.");
+    
     const req = {
         id: id.value,
         name: name.value,
@@ -25,7 +28,7 @@ function register() {
     .then((res) => res.json())
     .then((res) => {
         if (res.success === true) {
-            location.href ="/";
+            location.href ="/login";
         } else {
             alert(res.msg);
         }
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -19,30 +19,15 @@ const process = {
         const user = new User(req.body);
         const response = user.login();
         return res.json(response);
-        // const id = req.body.id;
-        // const password = req.body.password;
-        
-        // const users = UserStorage.getUsers("id", "password");
-        // const response = {};
-        // if (users.id.includes(id)) {
-        //     const idx = users.id.indexOf(id);
-        //     if (users.password[idx] === password) {
-        //         response.success = true;
-        //         return res.json(response);
-        //     }
-        // }
-
-        // response.success = false;
-        // response.msg = "로그인에 실패하셨습니다";
-        // return res.json(response)
     },
+    register: (req, res) => {
+        const user = new User(req.body);
+        const response = user.register();
+        return res.json(response);
+    }
 };
 
 module.exports = {
     output,
     process,
 }
-
-
-
-// render를 정확히 이해하기.
