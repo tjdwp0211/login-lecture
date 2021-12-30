bce0ff648c73c53fbe3b76347626a9d4b2cb2c70
김성제 <rlatjdwp0211@naver.com>
2021-12-30 13:20:24 +0900

    fs 모듈을 통한 로그인 기능 구현

--- app/src/models/User.js
+++ app/src/models/User.js
@@ -7,16 +7,17 @@ class User {
         this.body = body;
     }
 
-    login() {
+    async login() {
         const client = this.body;
-        const { id, password } = UserStorage.getUserInfo(client.id);
+        const { id, password } = await UserStorage.getUserInfo(client.id);
+        
         if (id) {
             if (id === client.id && password === client.password) {
             return { success: true };
+            }
+            return { success: false, msg: "비밀번호가 틀렸습니다." };
         }
-        return { success: false, msg: "비밀번호가 틀렸습니다." };
-    }
-    return { success: false, msg: "존재하지 않는 아이디입니다."};
+        return { success: false, msg: "존재하지 않는 아이디입니다."};
     }
 
     register() {
--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,14 +1,11 @@
 "use strict";
 
-class UserStorage {
-    static #users = {
-        id: ["kimsungje", "응애", "hihihi"],
-        password: ["1234", "1234", "123456"],
-        name: ["김성제", "애기", "인사쟁이"],
-    };
+const fs = require("fs").promises;
 
+class UserStorage {
+    
     static getUsers(...fields) {
-        const users = this.#users;
+        // const users = this.#users;
         const newUsers = fields.reduce((newUsers, field) => {
             if (users.hasOwnProperty(field)) {
                 newUsers[field] = users[field];
@@ -18,23 +15,34 @@ class UserStorage {
         return newUsers;
     }
     static getUserInfo(id) {
-        const users = this.#users;
+        return fs
+        .readFile("./src/databases/users.json")
+        .then((data) => {
+            return this.#getUserInfo(data, id)
+        })
+        .catch(console.error)
+    }
+    static #getUserInfo(data, id) {
+        const users = JSON.parse(data);
         const idx = users.id.indexOf(id);
         const usersKeys = Object.keys(users);
         const userInfo = usersKeys.reduce((newUser, info) => {
             newUser[info] = users[info][idx];
             return newUser;
         }, {});
+
         return userInfo;
     }
 
     static save(userInfo) {
-        const users = this.#users;
+        // const users = this.#users;
         users.id.push(userInfo.id);
         users.name.push(userInfo.name);
         users.password.push(userInfo.password);
         return { success : true };
-    }
+    }    
 }
+
+
 
 module.exports = UserStorage;
